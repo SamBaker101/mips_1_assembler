e8a38c5aa1ddafddeb04f2b823288f4e38d114b4
Sam Baker <Sam.Baker101@gmail.com>
2024-09-18 17:09:30 -0400

    Update Notes

--- readme.mk
+++ readme.mk
@@ -1,4 +1,23 @@
-I have been tossing around the idea of implementing a MIPS1 chip similar to the R3000 (I have a lot of nostalgia for a certian grey box from the 90s). 
+I have been tossing around the idea of implementing a MIPS1 chip similar to the R3000 (I have a lot of nostalgia for a certian grey box from the 90s). Additionally, it seems is the right complexity level to challenge me without becoming unmanagable. 
 Unfortunately I haven't worked much with the MIPS_1 architecture directly so I wanted to try to create an assembler to familiarize myself with the ISA before diving into a larger project using it.
 
 I will update this documentation when the assembler is up and running but for the moment assume it is non-functional.
+
+Some notes about MIPS_1:
+	- Registers
+		- 32 32-bit General Purpose Registers
+		- $0 - hardwired to zero
+		- HI/LO - 32-bit registers used for asynchronous int mul/div
+		- PC
+
+	- Instruction Formats:
+		- R type : Opcode[0:5], rs[6:10], rt[11:15], rd[16:20], shamt[21:25], funct[26:31] 
+		- I type : Opcode[0:5], rs[6:10], rt[11:15], immediate[16:31]
+		- J type : Opcode[0:5], Address[6:31]
+
+	- Instruction Details
+		- Load/Store
+			- 8-bit, 16-bit and 32-bit
+			- 
+		- Add/Sub
+		
