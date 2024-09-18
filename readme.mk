320bdd189f36d8adb24f5d56402f3051b9d2612a
Sam Baker <Sam.Baker101@gmail.com>
2024-09-18 12:25:51 -0400

    Add readme

---
+++ readme.mk
@@ -0,0 +1,4 @@
+I have been tossing around the idea of implementing a MIPS1 chip similar to the R3000 (I have a lot of nostalgia for a certian grey box from the 90s). 
+Unfortunately I haven't worked much with the MIPS_1 architecture directly so I wanted to try to create an assembler to familiarize myself with the ISA before diving into a larger project using it.
+
+I will update this documentation when the assembler is up and running but for the moment assume it is non-functional.
