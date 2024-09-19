83524819e47087b7d8d33dfe101de0e753171708
Sam Baker <Sam.Baker101@gmail.com>
2024-09-19 14:07:28 -0400

    Update Notes

--- readme.mk
+++ readme.mk
@@ -1,4 +1,5 @@
 I have been tossing around the idea of implementing a MIPS1 chip similar to the R3000 (I have a lot of nostalgia for a certian grey box from the 90s). Additionally, it seems is the right complexity level to challenge me without becoming unmanagable. 
+
 Unfortunately I haven't worked much with the MIPS_1 architecture directly so I wanted to try to create an assembler to familiarize myself with the ISA before diving into a larger project using it.
 
 I will update this documentation when the assembler is up and running but for the moment assume it is non-functional.
@@ -16,8 +17,26 @@ Some notes about MIPS_1:
 		- J type : Opcode[0:5], Address[6:31]
 
 	- Instruction Details
+		- Single point uses .s suffix
+		- Double uses .d suffix
+	
 		- Load/Store
 			- 8-bit, 16-bit and 32-bit
 			- 
 		- Add/Sub
+			- Source operands from RS and RT
+			- Output into RD
+			
+		- Mul/Div 
+			- Signed or Unsigned
+			- Output to HI/LOW 
+
+		- Control Flow
+			- Followed by branch delay slot
+			- Compare RS against zero or RT
+
+		-System Call, Breakpoint
+			- call exceptions
+
+	
 		
