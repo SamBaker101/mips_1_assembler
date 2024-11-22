96f781c021a5670a7a26e0b68db547778150d044
Sam Baker <Sam.Baker101@gmail.com>
2024-11-22 15:26:39 -0500

    bug fix, pass parser into parser class tests to avoid outfile being overwritten

--- tests/test.rb
+++ tests/test.rb
@@ -13,7 +13,7 @@ require "./src/instruction_c.rb"
 require "./src/data_c.rb"
 
 $COMMENT_CHARACTER   = "#"
-$HEX_OUT             = 1
+$INST_OFFSET         = "0x00400000"
 
 $IN         = "samples/sample1.asm"
 $INST_OUT   = "output/inst_out.txt"
@@ -41,10 +41,10 @@ def call_tests()
     #test_parse_file() 
 
     ### ParseC Tests ###
-    test_load_all_maps()
-    test_fill_queues()
-    test_check_for_mode_update()
-    test_update_line_class()
+    test_load_all_maps(parser)
+    test_fill_queues(parser)
+    test_check_for_mode_update(parser)
+    test_update_line_class(parser)
     #test_parse_input()     these will be part of test_parse_file
     #test_print_to_file()   these will be part of test_parse_file
     #test_print_labels()    these will be part of test_parse_file
@@ -90,8 +90,7 @@ end
 
     #load_all_maps()
     #load_map(map_path, map, index)
-def test_load_all_maps()
-    parser = ParseC.new()
+def test_load_all_maps(parser)
     parser.load_all_maps()
     if ($INSTRUCTION_MAP[0][0] != "ADD")
         abort("ERROR: #{__method__.to_s}, $INSTRUCTION_MAP[0][0] = #{$INSTRUCTION_MAP[0][0]}")
@@ -103,11 +102,9 @@ def test_load_all_maps()
         puts "TEST: #{__method__.to_s}: Completed Successfully"
     end
 end
-
     
     #fill_queues()
-def test_fill_queues()
-    parser = ParseC.new()
+def test_fill_queues(parser)
     parser.fill_queues(File.new($IN, "r"))
     if (parser.get_from_read_q(0).chomp != "#THIS IS A BASIC SANITY")
         abort("ERROR: #{__method__.to_s}, read_q[0] = #{parser.get_from_read_q(0)}")
@@ -118,8 +115,7 @@ def test_fill_queues()
 end
 
     #check_for_mode_update(line_for_check)
-def test_check_for_mode_update()
-    parser = ParseC.new()
+def test_check_for_mode_update(parser)
     mode_list = [[".text"], [".rdata"], [".data"], [".lit4"], [".lit8"], [".bss"], [".sdata"], [".sbss"]]
     (0..(mode_list.size() - 1)).each do |n|
         line = LineC.new(mode_list[n])
@@ -132,8 +128,7 @@ def test_check_for_mode_update()
 end
 
     #update_line_class(array, mode)
-def test_update_line_class()
-    parser = ParseC.new()
+def test_update_line_class(parser)
     class_list = ["InstructionC", "DataC", "DataC"]
     (0..(class_list.size() - 1)).each do |n|
         line = LineC.new($test_array_random)
