1b9e90c528ef947f4f4cc62d6bb84667c1728340
Sam Baker <Sam.Baker101@gmail.com>
2024-11-22 14:34:08 -0500

    updated prints to use __method__

--- tests/test.rb
+++ tests/test.rb
@@ -94,13 +94,13 @@ def test_load_all_maps()
     parser = ParseC.new()
     parser.load_all_maps()
     if ($INSTRUCTION_MAP[0][0] != "ADD")
-        abort("ERROR: load_all_maps(), $INSTRUCTION_MAP[0][0] = #{$INSTRUCTION_MAP[0][0]}")
+        abort("ERROR: #{__method__.to_s}, $INSTRUCTION_MAP[0][0] = #{$INSTRUCTION_MAP[0][0]}")
     elsif ($DIRECTIVE_MAP[0][0] != ".text")
-        abort("ERROR: load_all_maps(), $DIRECTIVE_MAP[0][0] = #{$DIRECTIVE_MAP[0][0]}")
+        abort("ERROR: #{__method__.to_s}, $DIRECTIVE_MAP[0][0] = #{$DIRECTIVE_MAP[0][0]}")
     elsif ($MNEMONIC_MAP[0][0] != "MOVE")
-        abort("ERROR: load_all_maps(), $MNEMONIC_MAP[0][0] = #{$MNEMONIC_MAP[0][0]}")
+        abort("ERROR: #{__method__.to_s}, $MNEMONIC_MAP[0][0] = #{$MNEMONIC_MAP[0][0]}")
     else
-        puts "TEST: load_all_maps(): Completed Successfully"
+        puts "TEST: #{__method__.to_s}: Completed Successfully"
     end
 end
 
@@ -110,9 +110,9 @@ def test_fill_queues()
     parser = ParseC.new()
     parser.fill_queues(File.new($IN, "r"))
     if (parser.get_from_read_q(0).chomp != "#THIS IS A BASIC SANITY")
-        abort("ERROR: fill_queues(), read_q[0] = #{parser.get_from_read_q(0)}")
+        abort("ERROR: #{__method__.to_s}, read_q[0] = #{parser.get_from_read_q(0)}")
     else
-        puts "TEST: fill_queues(): Completed Successfully"
+        puts "TEST: #{__method__.to_s}: Completed Successfully"
     end
 
 end
@@ -125,10 +125,10 @@ def test_check_for_mode_update()
         line = LineC.new(mode_list[n])
         parser.check_for_mode_update(line)
         if (parser.get_mode() != n)
-            abort("ERROR: check_for_mode_update(), expected = #{n}:#{mode_list[n]}, got #{parser.get_mode()}")
+            abort("ERROR: #{__method__.to_s}, expected = #{n}:#{mode_list[n]}, got #{parser.get_mode()}")
         end
     end
-    puts "TEST: check_for_mode_update(): Completed Successfully"
+    puts "TEST: #{__method__.to_s}: Completed Successfully"
 end
 
     #update_line_class(array, mode)
@@ -139,10 +139,10 @@ def test_update_line_class()
         line = LineC.new($test_array_random)
         line = parser.update_line_class($test_array_random, n)
         if (line.class().to_s != class_list[n])
-            abort("ERROR: update_line_class(), expected = #{n}:#{class_list[n]}, got #{line.class()}")
+            abort("ERROR: #{__method__.to_s}, expected = #{n}:#{class_list[n]}, got #{line.class()}")
         end
     end
-    puts "TEST: update_line_class(): Completed Successfully"
+    puts "TEST: #{__method__.to_s}: Completed Successfully"
 end
 
 #### class LineC ####
@@ -151,9 +151,9 @@ end
 def test_get_array(line)
     output_array = line.get_array()
     if (output_array != $test_array_random)
-        abort("ERROR: get_array error, input = #{$test_array_random}, out= #{output_array}")
+        abort("ERROR: #{__method__.to_s} error, input = #{$test_array_random}, out= #{output_array}")
     else
-        puts "TEST: get_array(): Completed Successfully"
+        puts "TEST: #{__method__.to_s}: Completed Successfully"
     end
 end
 
@@ -166,9 +166,9 @@ def test_set_get_output(line)
     line.set_output(test_output_bin)
     output = line.get_output()
     if (output != test_output_hex)
-        abort("ERROR: set_get_output error, input = #{test_output_bin}:#{test_output_hex}, out= #{output}")
+        abort("ERROR: #{__method__.to_s} error, input = #{test_output_bin}:#{test_output_hex}, out= #{output}")
     else
-        puts "TEST: set_get_output(): Completed Successfully"
+        puts "TEST: #{__method__.to_s}: Completed Successfully"
     end
 end
 
@@ -181,25 +181,25 @@ def test_detect_format_and_convert(line)
     
     hex2bin = line.detect_format_and_convert(hex)
     if (hex2bin != bin)
-        abort("ERROR: detect_format_and_convert - hex, input = #{hex}:#{bin}, out= #{hex2bin}")
+        abort("ERROR: #{__method__.to_s} - hex, input = #{hex}:#{bin}, out= #{hex2bin}")
     end
 
     dec2bin = line.detect_format_and_convert(dec)
     if (dec2bin != bin)
-        abort("ERROR: detect_format_and_convert - dec, input = #{dec}:#{bin}, out= #{dec2bin}")
+        abort("ERROR: #{__method__.to_s} - dec, input = #{dec}:#{bin}, out= #{dec2bin}")
     end
 
     bin2bin = line.detect_format_and_convert(bin)
     if (bin2bin != bin)
-        abort("ERROR: detect_format_and_convert - bin, input = #{bin}:#{bin}, out= #{bin2bin}")
+        abort("ERROR: #{__method__.to_s} - bin, input = #{bin}:#{bin}, out= #{bin2bin}")
     end
 
     oct2bin = line.detect_format_and_convert(oct)
     if (oct2bin != bin)
-        abort("ERROR: detect_format_and_convert - oct, input = #{oct}:#{bin}, out= #{oct2bin}")
+        abort("ERROR: #{__method__.to_s} - oct, input = #{oct}:#{bin}, out= #{oct2bin}")
     end
 
-    puts "TEST: detect_format_and_convert: Completed Successfully"
+    puts "TEST: #{__method__.to_s}: Completed Successfully"
 end
 
 #   def binary_encode(dec, bits = 32)    
@@ -211,9 +211,9 @@ def test_binary_encode(line)
     end
     output = line.binary_encode(dec)
     if (output != bin)
-        abort("ERROR: binary_encode, input = #{dec}, out= #{bin}")
+        abort("ERROR: #{__method__.to_s}, input = #{dec}, out= #{bin}")
     else
-        puts "TEST: binary_encode(): Completed Successfully"
+        puts "TEST: #{__method__.to_s}: Completed Successfully"
     end
 end
 
@@ -223,9 +223,9 @@ def test_binary_to_hex(line)
     bin = (hex.to_i(16)).to_s(2)
     output = line.binary_to_hex(bin)
     if (output != hex)
-        abort("ERROR: binary_to_hex, input = #{bin}, out= #{hex}")
+        abort("ERROR: #{__method__.to_s}, input = #{bin}, out= #{hex}")
     else
-        puts "TEST: binary_to_hex(): Completed Successfully"
+        puts "TEST: #{__method__.to_s}: Completed Successfully"
     end
 end
 
@@ -233,55 +233,55 @@ end
 def test_is_directive()
     line = LineC.new(["rd", "t3", "10"])
     if (line.is_directive() != 0)
-        abort(abort("ERROR: is_directive, input = #{line.get_array()}, out= 1"))
+        abort(abort("ERROR: #{__method__.to_s}, input = #{line.get_array()}, out= 1"))
     end
     line = LineC.new([".rdata", "101011"])
     if (line.is_directive() != 1)
-        abort(abort("ERROR: is_directive, input = #{line.get_array()}, out= 0"))
+        abort(abort("ERROR: #{__method__.to_s}, input = #{line.get_array()}, out= 0"))
     end
-    puts "TEST: is_directive(): Completed Successfully"
+    puts "TEST: #{__method__.to_s}: Completed Successfully"
 end
 
 #   def is_empty()
 def test_is_empty()
     line = LineC.new(["rd", "t3", "10"])
     if (line.is_empty() != 0)
-        abort(abort("ERROR: is_empty, input = #{test_line.get_array()}, out= #{1}"))
+        abort(abort("ERROR: #{__method__.to_s}, input = #{test_line.get_array()}, out= #{1}"))
     end
     line = LineC.new(["#This is a comment"])
     if (line.is_directive() != 1)
-        abort(abort("ERROR: is_directive, input = #{test_line.get_array()}, out= #{1}"))
+        abort(abort("ERROR: #{__method__.to_s}, input = #{test_line.get_array()}, out= #{1}"))
     end
-    puts "TEST: is_directive(): Completed Successfully"
+    puts "TEST: #{__method__.to_s}: Completed Successfully"
 end
     
 #   def decode_directive()
 def test_decode_directive_mode()    
     line = LineC.new([".data", "101011"])
     if (line.decode_directive_mode() != Mode::DATA)
-        abort(abort("ERROR: decode_directive, input = #{line.get_array()}"))
+        abort(abort("ERROR: #{__method__.to_s}, input = #{line.get_array()}"))
     end
     line = LineC.new([".rdata", "101011"])
     if (line.decode_directive_mode() != Mode::RDATA)
-        abort(abort("ERROR: decode_directive, input = #{line.get_array()}"))
+        abort(abort("ERROR: #{__method__.to_s}, input = #{line.get_array()}"))
     end
     line = LineC.new([".text", "101011"])
     if (line.decode_directive_mode() != Mode::TEXT)
-        abort(abort("ERROR: decode_directive, input = #{line.get_array()}"))
+        abort(abort("ERROR: #{__method__.to_s}, input = #{line.get_array()}"))
     end
-    puts "TEST: decode_directive(): Completed Successfully"
+    puts "TEST: #{__method__.to_s}: Completed Successfully"
 end
 
 # check_for_labels
 def test_check_for_labels()
     line = LineC.new(["test_label:"])
     if (line.check_for_labels() == 0);
-        abort("ERROR: check_for_labels() did not identify test label")
+        abort("ERROR: #{__method__.to_s} did not identify test label")
     end
 
     line = LineC.new($test_array_random)
     if (line.check_for_labels() != 0);
-        abort("ERROR: check_for_labels() identified non_label")
+        abort("ERROR: #{__method__.to_s} identified non_label")
     else
         puts "TEST: #{__method__.to_s}: Completed Successfully"
     end
