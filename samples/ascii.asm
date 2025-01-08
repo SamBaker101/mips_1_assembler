71ad2c3fcff3eb90a8f64c897f5d5e8339967c81
Sam Baker <Sam.Baker101@gmail.com>
2025-01-08 11:22:32 -0500

    Wrote out ascii sample code (untested)

--- samples/ascii.asm
+++ samples/ascii.asm
@@ -13,3 +13,38 @@
 #	chain[5] = 0;
 #	print_chain(chain);
 #}
+
+.data
+chain:	.byte -1, -1, -1, -1, -1, -1
+vec;	.word  5, 6, 8, 9, 1
+
+.text
+.globl main
+main:
+	li $s0 0		# i = 0
+while:
+	li $t0 5
+	bge $s0 $t0 end 	#while(i<5)
+	
+	li $ti 4
+	sub $t1 $t1 $s0		#4 - i
+	la $s1 vec
+	sll $t1 $t1 2		#4(4 - i)
+	addu $s1 $s1 $t1	#&vec + 4(4 - i)
+	
+	lbu $s2 0($s1)		
+	addiu $s2 $s2 48
+	la $s3 chain		
+	addu $s3 $s3 $s0	#chain + i
+	sb $s2 0($s3)		#chain[i] = vec[4-1] + '0'
+	addiu $s0 $s0 1
+	b while
+	
+fi:
+	la $t5 chain
+	sb $zero 5($t5)
+	li $v0 4
+	la $a0 chain
+	
+	jr $ra
+	
